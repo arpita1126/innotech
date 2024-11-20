d67ccfa09e1ff371a0fd8445538dd4b9168b02b1
arpita <arpita.2226csit1071@kiet.edu>
2024-11-20 23:45:29 +0530

    technocrts1

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.cta-btn').addEventListener('click', () => {
+    alert('Getting Started!');
+  });
+  
+  document.querySelector('.contact-btn').addEventListener('click', () => {
+    alert('Contact Us clicked!');
+  });
+  
